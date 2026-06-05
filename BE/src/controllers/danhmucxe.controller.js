9c6cbb47ada69aa62ca09a597751c827ce960956
Tran Tuan Hung <hungtran092006@gmail.com>
2026-06-05 13:22:01 +0700

    update danhmuc

--- BE/src/controllers/danhmucxe.controller.js
+++ BE/src/controllers/danhmucxe.controller.js
@@ -1,30 +1,30 @@
 import asyncHandler from "../utils/asyncHandler";
-import Article from "../models/danhmucxe.model";
+import Danhmuc from "../models/danhmucxe.model";
 
 export const getAll = asyncHandler(async (req, res) => {
-    const articles = await Article.find();
-    return res.json(articles)
+    const danhMucs = await Danhmuc.find();
+    return res.json(danhMucs)
 })
 export const getOne = asyncHandler(async (req, res) => {
-    const car = await Article.findById(req.params.id);
+    const danhMuc = await Danhmuc.findById(req.params.id);
 
-    if (!car) {
+    if (!danhMuc) {
         return res.status(404).json({
-            message: "Không tìm thấy xe"
+            message: "Không tìm thấy danh mục"
         });
     }
 
-    return res.json(car);
+    return res.json(danhMuc);
 });
 export const createOne = asyncHandler(async (req, res) => {
-    const article = await Article.create(req.body);
-    return res.json(article)
+    const danhMuc = await Danhmuc.create(req.body);
+    return res.json(danhMuc)
 })
 export const updateOne = asyncHandler(async (req, res) => {
-    const article = await Article.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    return res.json(article)
+    const danhMuc = await Danhmuc.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    return res.json(danhMuc)
 })
 export const deleteOne = asyncHandler(async (req, res) => {
-    const article = await Article.findByIdAndDelete(req.params.id);
-    return res.json(article)
+    const danhMuc = await Danhmuc.findByIdAndDelete(req.params.id);
+    return res.json(danhMuc)
 })
--- BE/src/routers/index.js
+++ BE/src/routers/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import busRouter from "./bus.router";
+import danhmucxeRouter from "./danhmucxe.router";   
 
 const router = Router();
 
 router.use("/bus", busRouter);
+router.use("/danhmucxe",danhmucxeRouter);
 
 export default router;
