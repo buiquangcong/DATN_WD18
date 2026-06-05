0a3f4bb6ef1555fe850d77185af6ce4f0623a175
buiquangcong <buicong908@gmail.com>
2026-06-05 08:23:09 +0700

    update base backend

--- BE/src/validations/article.validation.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-
-const schema = Joi.object({
-    title: Joi.string().required(),
-    content: Joi.string().required(),
-    author: Joi.string().required(),
-    createdAt: Joi.date()
-})
